2ea3f6a9ad04b30bffc53bc489e1a8626ad1bb29
ReForum-ipfs <buxianggaosuni@tutanota.com>
2021-01-12 14:16:03 +0800

    env

--- config/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
-  DBURL: 'mongodb://localhost:27017/reforum',
-};
--- frontend/env.js
+++
@@ -1,4 +0,0 @@
-
-export default {
-    url:'http://localhost:8080'    
-}
