eb98723fa8be9e38a4b7f033b655aaf349899ca8
ReForum-ipfs <buxianggaosuni@tutanota.com>
2021-02-07 15:49:47 +0800

    navbar update

--- frontendNew/src/Containers/Header/index.js
+++ frontendNew/src/Containers/Header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { hashHistory } from 'react-router';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 
@@ -10,19 +11,61 @@ import UserMenu from 'Components/Header/UserMenu';
 import Logo from 'Components/Header/Logo';
 import NavigationBar from 'Components/Header/NavigationBar';
 import PlaceholderImage from 'SharedStyles/placeholder.jpg';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    const { forums} = this.props;
+    if (forums){
+      this.state = { value: 0};
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange (event, newValue){
+    const { forums} = this.props;
+    this.setState({ value: newValue });
+    hashHistory.push(`/${forums[newValue].forum_slug}`);
+  };
   renderNavLinks() {
-    const { forums } = this.props;
-
+    const { forums} = this.props;
+    
+    const { value} = this.state;
     if (forums) {
-      return forums.map((forum) => {
-        return {
-          id: forum._id,
-          name: forum.forum_name,
-          link: `/${forum.forum_slug}`,
-        };
-      });
+        return (
+          <div style={{marginTop:'20px',marginBottom:'20px'}}>
+          <AppBar position="static"  color="default">
+            <Tabs
+              indicatorColor="primary"
+              textColor="primary"
+              variant="scrollable"
+              scrollButtons="auto"
+              value={value}
+              onChange={this.handleChange}
+              aria-label="nav tabs example"
+            >
+            {
+              forums.map((forum,index) => {
+                return <LinkTab value ={index} label={forum.forum_name} href={forum.forum_slug}/>
+              })
+            }
+            </Tabs>
+          </AppBar>
+          </div>
+        )
     }
 
     return null;
@@ -47,9 +90,11 @@ class Header extends Component {
             avatar={avatarUrl}
           />
         </div>
-        <NavigationBar
-          navigationLinks={this.renderNavLinks()}
-        />
+        
+          {this.renderNavLinks()}
+        {/* <NavigationBar
+          navigationLinks={}
+        /> */}
       </div>
     );
   }
